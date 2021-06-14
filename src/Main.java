e46e7b1444eef74d1aac59ddd9e5708c407525fe
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2021-06-14 16:23:22 +0600

    Faster And Bit Smarter

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -6,15 +7,72 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+
+    public static void main(String[] args) {
+        String storeFilePath = "Math130.txt";
+        String htmlFilePath = "html.txt";
+//        build(storeFilePath, htmlFilePath);
+        texts("Teachers", "Students");
+    }
+
+    static void texts(String... string) {
+        try (Scanner sc = new Scanner(new FileReader("html.txt"))) {
+            Matcher matcher;
+            int i = 0;
+            boolean[] found = new boolean[string.length];
+            Arrays.fill(found, false);
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                int from = line.indexOf(string[i]);
+                if (from > 0) {
+                    if (i - 2 > string.length) break;
+                    String str = line.substring(from);
+                    int to = str.indexOf(string[i + 1]);
+                    if (to > 0) {
+                        String limitedString = new String(str.substring(0, to));
+                        matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(limitedString);
+                        while (matcher.find()) {
+                            if (!found[i]) {
+                                System.out.println(string[i] + "---");
+                            }
+                            System.out.println(matcher.group());
+                            found[i] = true;
+                        }
+                        str = new String(str.substring(to));
+                        i++;
+                    }
+                    matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(str);
+                    while (matcher.find()) {
+                        if (!found[i]) {
+                            System.out.println(string[i] + "---");
+                        }
+                        System.out.println(matcher.group());
+                        found[i]=true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void statistics(boolean[] booleans, String[] strings) {
+        for (int i = 0; i < booleans.length; i++) {
+            System.out.println((booleans[i]) ? "Found" : "");
+
+        }
+    }
+
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b", Pattern.CASE_INSENSITIVE);
 
-
     public static Set<String> validateAndGrabFromFile(String filePath) {
         Set<String> set = new HashSet<>();
+        String teachers = "Teachers";
+        String classmates = "Classmates";
+
         try (Scanner sc = new Scanner(new FileReader(filePath))) {
             Matcher matcher;
-
             while (sc.hasNextLine()) {
                 matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(sc.nextLine());
                 while (matcher.find()) {
@@ -59,11 +117,9 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        String storeFilePath = "Math130", extension = ".txt";
-
-        Set<String> previousSet = plainGrabEmailFromFile(storeFilePath + extension);
-        Set<String> newSet = validateAndGrabFromFile("html.txt");
+    static void build(String storeFilePath, String htmlFilePath) {
+        Set<String> previousSet = plainGrabEmailFromFile(storeFilePath);
+        Set<String> newSet = validateAndGrabFromFile(htmlFilePath);
         Set<String> commonSet = new HashSet<>();
 
         commonSet.addAll(previousSet);
@@ -71,15 +127,15 @@ public class Main {
 
         newSet.removeAll(previousSet);
 
-        writeToFile(commonSet, storeFilePath + extension);
+        writeToFile(commonSet, storeFilePath);
         if (newSet.size() > 0 && newSet.size() != commonSet.size()) {
-            writeToFile(newSet, storeFilePath + "_new" + extension);
+            writeToFile(newSet, "_new" + storeFilePath);
         }
         System.out.println(newSet.size());
         for (var o : newSet) {
             System.out.println(o);
         }
-
-
     }
+
+
 }
